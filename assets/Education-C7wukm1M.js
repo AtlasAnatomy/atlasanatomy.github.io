6decf4528999eca0d8dc57da63340622e23c620f
Tommaso Bosi <bositommaso13@gmail.com>
2026-09-01 07:48:38 +0200

    Deploy del sito compilato

--- assets/Education-C7wukm1M.js
+++
@@ -1,1 +0,0 @@
-import{j as t,d as e}from"./index-CXvKvQA1.js";import{S as o}from"./SectionWrapper-CYvmDgmI.js";import{S as i}from"./SectionHeading-DXctsO3W.js";import{T as s}from"./Timeline-j7kv80GH.js";const a=()=>t.jsxs(t.Fragment,{children:[t.jsx(i,{eyebrow:"Where I studied",title:"Education."}),t.jsx(s,{items:e})]}),c=o(a,"education");export{c as default};
--- assets/extends-Ck03K7n_.js
+++ assets/extends-Ck03K7n_.js
@@ -1,4 +1,4 @@
-import{r as Je,w as dx,j as vi}from"./index-CXvKvQA1.js";/**
+import{r as Je,w as dx,j as vi}from"./index-8fGWsCjO.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
